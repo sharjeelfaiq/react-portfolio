44dfd27339605fe5e99e3d547cf07c6d16ce183e
Sharjeel <mrmalik610@gmail.com>
2022-06-11 08:59:22 +0500

    Modified About.js.

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,20 +8,19 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
-            <br />I am a junior pursuing IMSC in Maths and Computing in BIT
-            Mesra.
+            Hi Everyone, I am <span className="purple">Sharjeel Faiq </span>
+            from <span className="purple"> Quetta, Pakistan.</span>
+            <br />I am an undergraduate in Mathematics and have choosen Web Development as my professional career.
             <br />
             <br />
             Apart from coding, some other activities that I love to do!
           </p>
           <ul>
             <li className="about-activity">
-              <ImPointRight /> Playing Games
+              <ImPointRight /> Teaching Mathematics
             </li>
             <li className="about-activity">
-              <ImPointRight /> Writting Tech Blogs
+              <ImPointRight /> Learning To Solve Complex Problems
             </li>
             <li className="about-activity">
               <ImPointRight /> Travelling
@@ -29,9 +28,9 @@ function AboutCard() {
           </ul>
 
           <p style={{ color: "rgb(155 126 172)" }}>
-            "Strive to build things that make a difference!"{" "}
+            "I can't change the direction of the wind, hut I can adjust my sails to always reach my destination!"{" "}
           </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
+          <footer className="blockquote-footer">Sharjeel</footer>
         </blockquote>
       </Card.Body>
     </Card>
