bb29cd5a3cf1d43c6bd8631b889d1633d0e9cba8
Sharjeel Faiq <mrmalik610@gmail.com>
2024-10-01 16:03:30 +0500

    segregated projects data into a seperate file.

---
+++ src/components/Projects/ProjectsData.js
@@ -0,0 +1,80 @@
+import pImg1 from "../../Assets/ProjectsImgs/PI_9.webp";
+import pImg2 from "../../Assets/ProjectsImgs/PI_10.webp";
+import pImg3 from "../../Assets/ProjectsImgs/PI_12.webp";
+import pImg4 from "../../Assets/ProjectsImgs/PI_15.webp";
+import pImg5 from "../../Assets/ProjectsImgs/PI_17.webp";
+import pImg6 from "../../Assets/ProjectsImgs/PI_19.webp";
+import pImg7 from "../../Assets/ProjectsImgs/PI_21.webp";
+import pImg8 from "../../Assets/ProjectsImgs/PI_22.webp";
+import pImg9 from "../../Assets/ProjectsImgs/PI_23.webp";
+
+export const projectsData = [
+  {
+    image: pImg1,
+    title: "TaskMate",
+    description: "React JS | Material UI",
+    responsiveness: "Mobile Responsive? Yes!",
+    link: "https://taskmate610.netlify.app/",
+    repo: "https://github.com/sharjeelfaiq/TaskMate",
+  },
+  {
+    image: pImg2,
+    title: "Textbotonline",
+    description: "React JS | Open-AI API | BootStrap 5.0",
+    responsiveness: "Mobile Responsive? Yes!",
+    link: "https://textbotonline.netlify.app/",
+    repo: "https://github.com/sharjeelfaiq/textbotonline.git",
+  },
+  {
+    image: pImg3,
+    title: "Pakistan Prayers",
+    description: "React JS | Al-Adhan API | Bootstrap 5.0",
+    responsiveness: "Mobile Responsive? No!",
+    link: "https://pakistanprayers.netlify.app/",
+    repo: "https://github.com/sharjeelfaiq/pakistanprayers",
+  },
+  {
+    image: pImg4,
+    title: "x-coin.network",
+    description: "HTML | CSS | JavaScript",
+    responsiveness: "Mobile Responsive? Yes!",
+    link: "https://x-coin-network.netlify.app/",
+  },
+  {
+    image: pImg5,
+    title: "Youngstarslabs",
+    description: "React JS | Tailwind CSS",
+    responsiveness: "Mobile Responsive? Yes!",
+    link: "https://youngstarslabs.netlify.app/",
+  },
+  {
+    image: pImg6,
+    title: "UMED LLC",
+    description: "HTML | CSS | JavaScript",
+    responsiveness: "Mobile Responsive? Yes!",
+    link: "https://umedllc.netlify.app/",
+  },
+  {
+    image: pImg7,
+    title: "TechTrip - Shopping Cart",
+    description: "React JS | Context API | Tailwind CSS | Ant Design",
+    responsiveness: "Mobile Responsive? Yes!",
+    link: "https://techtrip.netlify.app/",
+    repo: "https://github.com/sharjeelfaiq/techtrip.git",
+  },
+  {
+    image: pImg8,
+    title: "Random User Generator",
+    description: "React JS | Tailwind CSS | Random User Generator API",
+    responsiveness: "Mobile Responsive? Yes!",
+    link: "https://random-user-finder-16.netlify.app/",
+    repo: "https://github.com/sharjeelfaiq/random-user-finder",
+  },
+  {
+    image: pImg9,
+    title: "E-Commerce Dashboard",
+    description: "React | Node | Express | MongoDB | Tailwind CSS",
+    responsiveness: "Mobile Responsive? Yes!",
+    link: "https://e-com-16.netlify.app/",
+  },
+];
